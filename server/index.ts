66a47e670f8288b18baf37fc075c31a8285b775c
inam95 <mfihassan99@gmail.com>
2024-10-17 09:58:52 +0530

    Error handling

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,42 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
+
+import type { ErrorResponse } from "@/shared/types";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text(" Hello Hono!");
+});
+
+app.onError((error, c) => {
+  if (error instanceof HTTPException) {
+    const errResponse =
+      error.res ??
+      c.json<ErrorResponse>(
+        {
+          success: false,
+          message: error.message,
+          isFormError:
+            error.cause && typeof error.cause === "object" && "form" in error.cause
+              ? error.cause.form === true
+              : false,
+        },
+        error.status,
+      );
+
+    return errResponse;
+  }
+  return c.json<ErrorResponse>(
+    {
+      success: false,
+      message:
+        process.env.NODE_ENV === "production"
+          ? "Internal Server Error"
+          : (error.stack ?? error.message),
+    },
+    500,
+  );
 });
 
 export default app;
